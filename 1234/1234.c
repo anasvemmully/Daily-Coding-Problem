a99403e4525b568550d293c9042ea94763fa8908
anasvemmully <anasvemmully@gmail.com>
2022-12-02 07:58:28 +0530

    ⚠️ 1234 not solved!

---
+++ 1234/1234.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+int transform(int[], int);
+
+int main() {  
+
+    char qux[] = {'R', 'B', 'B', 'G', 'B', 'R', 'G', 'G'};
+
+    // this function spits out the length of the given array of quxes
+    // printf("\nThe size of the given array is %ld\n", sizeof(qux));
+    
+    printf("the smallest of them : %d", transform(qux, 1));
+
+    return 0;
+}
+
+int transform(int arr[], int merge_pos) {
+    
+    
+    
+
+}
